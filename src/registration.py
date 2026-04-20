7f2b9578673205ca4d322d81b5405233e9f23acf
J1mmortal <jespernielsmeijerink@gmail.com>
2026-04-20 15:19:50 +0200

    fusing global reg and local reg for standard icp

--- src/registration.py
+++ src/registration.py
@@ -1,33 +1,29 @@
 import numpy as np
 import open3d as o3d
 import copy
+from src.global_reg import Registration
+import plotly.io as pio
+pio.renderers.default = "browser"
 
+reg = Registration()
+
+threshold = 0.5
+max_iter = 100
 demo_pcs = o3d.data.DemoICPPointClouds()
-source = o3d.io.read_point_cloud(demo_pcs.paths[0]) # pc with 191397 points.
-target = o3d.io.read_point_cloud(demo_pcs.paths[1]) # pc with 137833 points.
-o3d.visualization.draw_plotly([source],
-                                  zoom=0.455,
-                                  front=[-0.4999, -0.1659, -0.8499],
-                                  lookat=[2.1813, 2.0619, 2.0999],
-                                  up=[0.1204, -0.9852, 0.1215])
- 
-o3d.visualization.draw_plotly([target],
-                                  zoom=0.455,
-                                  front=[-0.4999, -0.1659, -0.8499],
-                                  lookat=[2.1813, 2.0619, 2.0999],
-                                  up=[0.1204, -0.9852, 0.1215])
+src = o3d.io.read_point_cloud(demo_pcs.paths[0]) # pc with 191397 points.
+tgt = o3d.io.read_point_cloud(demo_pcs.paths[1]) # pc with 137833 points.
+#src = o3d.io.read_point_cloud("reference_scan.ply")
+#tgt = o3d.io.read_point_cloud("dented_scan.ply")
 
-#### Initial Guess for Transformation ####
-init_guess = np.asarray([[0.862, 0.011, -0.507, 0.5],
-                         [-0.139, 0.967, -0.215, 0.7],
-                         [0.487, 0.255, 0.835, -1.4], [0.0, 0.0, 0.0, 1.0]])
+#### Initial guess for transformation when global method not working####
+#init_guess = np.asarray([[0.862, 0.011, -0.507, 0.5], [-0.139, 0.967, -0.215, 0.7], [0.487, 0.255, 0.835, -1.4], [0.0, 0.0, 0.0, 1.0]])
 
 def preprocess_cloud(pc, voxel_size=None):
     pc = copy.deepcopy(pc)
     if voxel_size is not None:
         pc = pc.voxel_down_sample(voxel_size)
     pc.estimate_normals(o3d.geometry.KDTreeSearchParamHybrid(radius=2.0 * (voxel_size or 1.0), max_nn=30))
-    return
+    return pc
 
 def icp_refinement(source_pc, target_pc, init_guess, threshold=1.0, max_iter=100):
     criteria = o3d.pipelines.registration.ICPConvergenceCriteria(max_iteration=max_iter)
@@ -49,41 +45,27 @@ def visualise_result(source_pc, target_pc, transform, title="Results for unassig
     source_temp.paint_uniform_color([1, 0.2, 0])
     target_temp.paint_uniform_color([0, 0.65, 0.93])
     source_temp.transform(transform)
-    o3d.visualization.draw_plotly([source_temp, target_temp], window_name=title)
-    return
+    fig = o3d.visualization.draw_plotly([source_temp, target_temp], window_name=title)
+    return fig
 
-src = o3d.io.read_point_cloud("reference_scan.ply")
-tgt = o3d.io.read_point_cloud("dented_scan.ply")
-o3d.visualization.draw_plotly([source],
-                                  zoom=0.455,
-                                  front=[-0.4999, -0.1659, -0.8499],
-                                  lookat=[2.1813, 2.0619, 2.0999],
-                                  up=[0.1204, -0.9852, 0.1215])
- 
-o3d.visualization.draw_plotly([target],
-                                  zoom=0.455,
-                                  front=[-0.4999, -0.1659, -0.8499],
-                                  lookat=[2.1813, 2.0619, 2.0999],
-                                  up=[0.1204, -0.9852, 0.1215])
 
-source = preprocess_cloud(source, voxel_size=0.5)
-target = preprocess_cloud(target, voxel_size=0.5)
-init_guess = np.asarray([[0.862, 0.011, -0.507, 0.5],
-                         [-0.139, 0.967, -0.215, 0.7],
-                         [0.487, 0.255, 0.835, -1.4], [0.0, 0.0, 0.0, 1.0]])
+#### Work flow ####
+source = preprocess_cloud(src, voxel_size=0.5)
+target = preprocess_cloud(tgt, voxel_size=0.5)
+
+global_result = reg.get_initial_guess(src, tgt)
+init_guess = global_result.transformation
 
 print("Before ICP:")
-before_eval = evaluate_alignment(source, target, init_guess, threshold=1.0)
+visualise_result(source, target, init_guess)
+before_eval = evaluate_alignment(source, target, init_guess, threshold)
 print(before_eval)
 
-icp_result = icp_refinement(source, target, init_guess, threshold=1.0, max_iterations=100)
-
+icp_result = icp_refinement(source, target, init_guess, threshold, max_iter)
 print("After ICP:")
 print(icp_result)
 print("Transformation:")
 print(icp_result.transformation)
-
-after_eval = evaluate_alignment(source, target, icp_result.transformation, threshold=1.0)
+after_eval = evaluate_alignment(source, target, icp_result.transformation, threshold)
 print(after_eval)
-
 visualise_result(source, target, icp_result.transformation, title="ICP refinement")
